62d458bfd9ced0c866b84fe6cc996a20190eb7ae
rupertMme <rupert.heilmeier@stud.uni-regensburg.de>
2014-05-14 17:54:37 +0200

    Gun eingebaut und schuß auf Position der gun gelegt

--- PSMG_Alarm/Assets/PlayerShooting.cs
+++ PSMG_Alarm/Assets/PlayerShooting.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerShooting : MonoBehaviour {
     public Rigidbody2D rocket;
-    public float speed = 15f;
+    public float speed = 19f;
 
     private MovePlayer uboot;
     
@@ -14,11 +14,27 @@ public class PlayerShooting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
+        
+	}
+    void FixedUpdate()
+    {
+        Vector3 aimPositon = Input.mousePosition;
+        aimPositon.z = 0.0f;
+        Vector3 ubootposition = Camera.main.WorldToScreenPoint(transform.position);
+        aimPositon.x = aimPositon.x - ubootposition.x;
+        aimPositon.y = aimPositon.y - ubootposition.y;
+        float angle = Mathf.Atan2(aimPositon.y, aimPositon.x) * Mathf.Rad2Deg - 90;
+        Vector3 rotationVector = new Vector3(0, 0, angle);
+        transform.rotation = Quaternion.Euler(rotationVector);
+
         if (Input.GetButtonDown("Fire1"))
         {
-            Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
-            bulletInstance.velocity = new Vector2(speed, 0);
+            
+            Debug.Log("posx" + aimPositon.x + " posy " + aimPositon.y +"angle"+ angle);
+            Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, angle+90))) as Rigidbody2D;
+            bulletInstance.velocity = new Vector2(aimPositon.x / speed, aimPositon.y / speed);
 
         }
-	}
+    }
 }
