57a9f8e59a47ce66499233e121e758ffc7a4eec7
dudesoft <andreas@pritschet.eu>
2014-05-14 16:51:13 +0200

    EnemySpawner und DummieKI

---
+++ PSMG_Alarm/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public GameObject camera2d;
+
+    private int enemiesAlive;
+    public int enemiesSouldBeAlive;
+
+    void Update()
+    {
+        if (enemiesAlive < enemiesSouldBeAlive)
+            SpawnEnemy();
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        float screenX = Random.Range(0.0f, camera2d.camera.pixelWidth);
+        float screenY = Random.Range(0.0f, camera2d.camera.pixelHeight);
+        Vector3 randomPos = camera2d.camera.ScreenToWorldPoint(new Vector3(screenX, screenY, 9));
+
+        return randomPos;
+    }
+
+    private void SpawnEnemy()
+    {
+        Vector3 pos = GetRandomPosition();
+        GameObject clone = Instantiate(enemy, pos, Quaternion.EulerAngles(90, 0, 0)) as GameObject;
+        enemiesAlive++;
+    }
+
+    private void EnemyDied()
+    {
+        enemiesAlive--;
+    }
+}
