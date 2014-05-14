bd8da7b1f40bb7c50f77b795e63afa2a6abdd663
rupertMme <rupert.heilmeier@stud.uni-regensburg.de>
2014-05-15 00:00:45 +0200

    Vorimplementierung für Schuß

--- PSMG_Alarm/Assets/PlayerShooting.cs
+++ PSMG_Alarm/Assets/PlayerShooting.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 
 public class PlayerShooting : MonoBehaviour {
-    public Rigidbody2D rocket;
+    public GameObject[] rocket;
     public float speed = 19f;
+    
 
     private MovePlayer uboot;
     
@@ -21,19 +22,26 @@ public class PlayerShooting : MonoBehaviour {
     {
         Vector3 aimPositon = Input.mousePosition;
         aimPositon.z = 0.0f;
+        //Vector3 ubootposition = Camera.main.WorldToScreenPoint(transform.position);
         Vector3 ubootposition = Camera.main.WorldToScreenPoint(transform.position);
         aimPositon.x = aimPositon.x - ubootposition.x;
         aimPositon.y = aimPositon.y - ubootposition.y;
         float angle = Mathf.Atan2(aimPositon.y, aimPositon.x) * Mathf.Rad2Deg - 90;
         Vector3 rotationVector = new Vector3(0, 0, angle);
         transform.rotation = Quaternion.Euler(rotationVector);
+        
 
         if (Input.GetButtonDown("Fire1"))
         {
+            int ammoIndex = 0;
+            //Debug.Log("posx" + aimPositon.x + " posy " + aimPositon.y +"angle"+ angle);
+            GameObject bulletInstance = (GameObject)Instantiate(rocket[ammoIndex], transform.position,transform.rotation);
+
             
-            Debug.Log("posx" + aimPositon.x + " posy " + aimPositon.y +"angle"+ angle);
-            Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, angle+90))) as Rigidbody2D;
-            bulletInstance.velocity = new Vector2(aimPositon.x / speed, aimPositon.y / speed);
+            //bulletInstance.velocity = new Vector2(aimPositon.x / speed, aimPositon.y / speed);
+            Debug.Log("mouseposition" + aimPositon);
+            bulletInstance.transform.LookAt(aimPositon);
+           // bulletInstance.rigidbody2D.AddForce(bulletInstance.transform.forward * speed);
 
         }
     }
