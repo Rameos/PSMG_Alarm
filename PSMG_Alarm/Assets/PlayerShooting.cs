99904da21f1f49eb7744894f8b6ecb6083bf9d18
dudesoft <andreas@pritschet.eu>
2014-05-15 17:56:24 +0200

    Eyetracker integriert

--- PSMG_Alarm/Assets/PlayerShooting.cs
+++ PSMG_Alarm/Assets/PlayerShooting.cs
@@ -19,13 +19,14 @@ public class PlayerShooting : MonoBehaviour
 
     void Update()
     {
-        Vector3 aimPositon = Input.mousePosition;
+        Vector3 aimPositon = (gazeModel.posGazeLeft + gazeModel.posGazeRight) * 0.5f;
+         //  Vector3 aimPositon = Input.mousePosition;
         aimPositon.z = 0.0f;
         //Vector3 ubootposition = Camera.main.WorldToScreenPoint(transform.position);
         Vector3 ubootposition = Camera.main.WorldToScreenPoint(transform.position);
         aimPositon.x = aimPositon.x - ubootposition.x;
         aimPositon.y = aimPositon.y - ubootposition.y;
-        float angle = Mathf.Atan2(aimPositon.y, aimPositon.x) * Mathf.Rad2Deg - 90;
+        float angle = Mathf.Atan2(-aimPositon.y, aimPositon.x) * Mathf.Rad2Deg - 90;
         Vector3 rotationVector = new Vector3(0, 0, angle);
         transform.rotation = Quaternion.Euler(rotationVector);
 
