f1bec48a5cdf60c23217b3aaaeccbe847e0a1e59
MaxSchu <maximilian.schubert@stud.uni-regensburg.de>
2014-06-12 16:29:53 +0200

    Submarine Szene ergänzt

--- PSMG_Alarm/Assets/Scripts/GameOverScript.cs
+++ PSMG_Alarm/Assets/Scripts/GameOverScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class GameOverScript : MonoBehaviour {
 
 	public GUIText gameOverText;
+	public GUITexture tryAgainBut;
+	public GUITexture mainMenuBut;
 
 	// Use this for initialization
 	void Start () {
