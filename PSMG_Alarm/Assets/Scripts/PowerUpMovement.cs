0ac816888b85739434b90067337e80e22b6ddaa6
Laura-E <Laura.Edel@stud.uni-regensburg.de>
2014-06-12 16:53:06 +0200

    add powerUpSpawner-Script and powerUpMovement-Script

---
+++ PSMG_Alarm/Assets/Scripts/PowerUpMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpMovement : MonoBehaviour
+{
+
+    private Vector3 targetLocation;
+    public GameObject camera2d;
+    private PowerUpSpawner spawner;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = 0.2f;
+        camera2d = GameObject.Find("2D Camera");
+        spawner = GameObject.Find("GameController").GetComponent<PowerUpSpawner>();
+        GetNewTargetLocation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToTargetLocation();
+    }
+
+    void GetNewTargetLocation()
+    {
+        float screenX = Random.Range(0.0f, camera2d.camera.pixelWidth);
+        float screenY = Random.Range(0.0f, camera2d.camera.pixelHeight);
+        targetLocation = camera2d.camera.ScreenToWorldPoint(new Vector3(screenX, screenY, 9));
+        targetLocation.z = 9;
+    }
+
+    void MoveToTargetLocation()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetLocation, Time.deltaTime * speed);
+        if (transform.position == targetLocation)
+            GetNewTargetLocation();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            spawner.removePowerUp();
+        }
+    }
+}
