7e0bbfe957b32d52a22a7616ec2f2de580ce84d2
dudesoft <andreas@pritschet.eu>
2014-07-31 18:06:39 +0200

    Awesome visual updates for Skilltree

--- PSMG_Alarm/Assets/Scripts/GUI/SkilltreeGui.cs
+++ PSMG_Alarm/Assets/Scripts/GUI/SkilltreeGui.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SkilltreeGui : MonoBehaviour {
-
-    public Texture2D coin;
+public class SkilltreeGui : MonoBehaviour
+{
     public GUIStyle coinsCountStyle;
     public GUIStyle titleStyle;
     public GUIStyle upgradeDescription;
     public GUIStyle descriptionText;
 
-    private readonly int optimizedHeight = 768;
+    public static readonly int optimizedHeight = 768;
+    public static readonly int optimizedWidth = 1024;
+
+    private GameObject ammoPhaser;
+    private GameObject ammoRocket;
+    private GameObject coin;
 
     private string description;
     private string cost;
@@ -17,13 +21,22 @@ public class SkilltreeGui : MonoBehaviour {
 
     private int standardTitleSize;
     private int standardCointextSize;
+
+    private float standardCanvasSizeY;
+    private float standardCanvasSizeX;
+
     private bool hovering;
 
     void Start()
     {
         standardTitleSize = titleStyle.fontSize;
         standardCointextSize = coinsCountStyle.fontSize;
+
+        ammoPhaser = GameObject.Find("AmmoItemPhaser");
+        ammoRocket = GameObject.Find("AmmoItemRocket");
+        coin = GameObject.Find("Coin");
     }
+
 
     void OnGUI()
     {
@@ -33,12 +46,22 @@ public class SkilltreeGui : MonoBehaviour {
         titleStyle.alignment = TextAnchor.UpperCenter;
 
         coinsCountStyle.fontSize = Mathf.RoundToInt(standardCointextSize * ratioY);
-        coinsCountStyle.alignment = TextAnchor.UpperCenter;
 
-        GUI.Label(new Rect(5, 10, 100, 100), coin);
-        GUI.Box(new Rect(105, 40, 50, 30), "X 34", coinsCountStyle);
+        GUI.Box(new Rect(camera.WorldToScreenPoint(coin.transform.position).x + 30 * Screen.height / 500,
+            Screen.height - camera.WorldToScreenPoint(coin.transform.position).y - 15 + Screen.height / 500, 50, 30),
+            "x 134", coinsCountStyle);
 
-        GUI.Box(new Rect(Screen.width / 2 - 150, 10, 300, 50), "Get new gear!", titleStyle);
+        coinsCountStyle.alignment = TextAnchor.UpperLeft;
+
+        GUI.Box(new Rect(camera.WorldToScreenPoint(ammoPhaser.transform.position).x - 15 * Screen.height / 500,
+            camera.WorldToScreenPoint(ammoPhaser.transform.position).y + 50 * Screen.height / 500, 50, 30),
+            "34", coinsCountStyle);
+
+        GUI.Box(new Rect(camera.WorldToScreenPoint(ammoRocket.transform.position).x - 15 * Screen.height / 500,
+            camera.WorldToScreenPoint(ammoRocket.transform.position).y + 50 * Screen.height / 500, 50, 30),
+            "34", coinsCountStyle);
+
+        GUI.Box(new Rect(Screen.width / 13, Screen.height / 20, 300, 100), "Prepare For Your Next Mission...", titleStyle);
 
         if (hovering)
         {
@@ -46,7 +69,7 @@ public class SkilltreeGui : MonoBehaviour {
             {
                 GUILayout.BeginVertical(upgradeDescription); // also can put width in here
                 {
-                    GUILayout.Label(title + " (" + cost +")", descriptionText);
+                    GUILayout.Label(title + " (" + cost + ")", descriptionText);
                     GUILayout.Label(description, descriptionText);
                 }
                 GUILayout.EndVertical();
---
+++ PSMG_Alarm/Assets/Scripts/Other Stuff/AlarmEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmEffect : MonoBehaviour
+{
+
+    public float minIntensity;
+    public float maxIntensity;
+    public float speed = 1f;
+
+    void Update()
+    {
+        light.intensity += speed * Time.deltaTime;
+
+        if (light.intensity > maxIntensity || light.intensity < minIntensity)
+            speed *= -1;
+    }
+}
