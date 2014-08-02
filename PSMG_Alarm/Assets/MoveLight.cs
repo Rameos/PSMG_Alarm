5ddc335aafbb4753b608e9180f746140474b3266
Andreas Pritschet <apri@kupferwerk.net>
2014-08-02 15:35:24 +0200

    Improved lighting

---
+++ PSMG_Alarm/Assets/MoveLight.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLight : MonoBehaviour
+{
+
+    public float speedX = 0.5f;
+    public float speedY = 0.5f;
+
+    void Update()
+    {
+        transform.position += new Vector3(speedX * Time.deltaTime, speedY * Time.deltaTime, 0);
+    }
+}
