43bc009c6de4b32c7a9b3b5ba2d5d281ea39eadb
Rameos <martin.dechant@web.de>
2014-09-14 21:51:20 +0200

    Test Basic Callback

--- PSMG_Alarm/Assets/Plugin/Components/GazeController.cs
+++ PSMG_Alarm/Assets/Plugin/Components/GazeController.cs
@@ -210,51 +210,52 @@ namespace iViewX
         /// <param name="sampleData"></param>
         private void GetSampleCallbackFunction(EyeTrackingController.SampleStruct sampleData)
         {
+            Debug.Log("sampleData:LeftEye PosX:" + sampleData.leftEye.eyePositionX);
 
-            //Left Eye
-            gazeModel.posLeftEye = new Vector3((float)sampleData.leftEye.eyePositionX, (float)sampleData.leftEye.eyePositionY, (float)sampleData.leftEye.eyePositionZ);
-            gazeModel.posGazeLeft = new Vector2((float)sampleData.leftEye.gazeX, (float)sampleData.leftEye.gazeY);
+//            //Left Eye
+//            gazeModel.posLeftEye = new Vector3((float)sampleData.leftEye.eyePositionX, (float)sampleData.leftEye.eyePositionY, (float)sampleData.leftEye.eyePositionZ);
+//            gazeModel.posGazeLeft = new Vector2((float)sampleData.leftEye.gazeX, (float)sampleData.leftEye.gazeY);
 
-            //Right Eye
-            gazeModel.posRightEye = new Vector3((float)sampleData.rightEye.eyePositionX, (float)sampleData.rightEye.eyePositionY, (float)sampleData.rightEye.eyePositionZ);
-            gazeModel.posGazeRight = new Vector2((float)sampleData.rightEye.gazeX, (float)sampleData.rightEye.gazeY);
+//            //Right Eye
+//            gazeModel.posRightEye = new Vector3((float)sampleData.rightEye.eyePositionX, (float)sampleData.rightEye.eyePositionY, (float)sampleData.rightEye.eyePositionZ);
+//            gazeModel.posGazeRight = new Vector2((float)sampleData.rightEye.gazeX, (float)sampleData.rightEye.gazeY);
 
-            //PupilData
-            gazeModel.diamLeftEye = (float)sampleData.leftEye.diam;
-            gazeModel.diamRightEye = (float)sampleData.leftEye.diam;
+//            //PupilData
+//            gazeModel.diamLeftEye = (float)sampleData.leftEye.diam;
+//            gazeModel.diamRightEye = (float)sampleData.leftEye.diam;
 
 
 
-#if UNITY_EDITOR
-            Rect gameView = getOffsetFromGameView();
-            Vector2 posGazeLeft = new Vector3(gazeModel.posGazeLeft.x, gazeModel.posGazeLeft.y - gameView.y);
-            Vector2 posGazeRight = new Vector3(gazeModel.posGazeRight.x, gazeModel.posGazeRight.y - gameView.y);
+//#if UNITY_EDITOR
+//            Rect gameView = getOffsetFromGameView();
+//            Vector2 posGazeLeft = new Vector3(gazeModel.posGazeLeft.x, gazeModel.posGazeLeft.y - gameView.y);
+//            Vector2 posGazeRight = new Vector3(gazeModel.posGazeRight.x, gazeModel.posGazeRight.y - gameView.y);
 
-            gazeModel.posGazeLeft = posGazeLeft;
-            gazeModel.posGazeRight = posGazeRight;
+//            gazeModel.posGazeLeft = posGazeLeft;
+//            gazeModel.posGazeRight = posGazeRight;
 
-#else
-            Vector2 offSet = Win32HelperClass.GetGameViewPosition();
-            if (offSet.x >= 0)
-            {
-                gazeModel.posGazeLeft = gazeModel.posGazeLeft - offSet;
-                gazeModel.posGazeRight = gazeModel.posGazeRight - offSet;
-            }
-            else
-            {
-                gazeModel.posGazeLeft = offSet -gazeModel.posGazeLeft;
-                gazeModel.posGazeRight = offSet - gazeModel.posGazeRight;
-            }
+//#else
+//            Vector2 offSet = Win32HelperClass.GetGameViewPosition();
+//            if (offSet.x >= 0)
+//            {
+//                gazeModel.posGazeLeft = gazeModel.posGazeLeft - offSet;
+//                gazeModel.posGazeRight = gazeModel.posGazeRight - offSet;
+//            }
+//            else
+//            {
+//                gazeModel.posGazeLeft = offSet -gazeModel.posGazeLeft;
+//                gazeModel.posGazeRight = offSet - gazeModel.posGazeRight;
+//            }
 
-#endif
+//#endif
 
-            gazeModel.gameScreenPosition = Win32HelperClass.GetGameViewPosition();
+//            gazeModel.gameScreenPosition = Win32HelperClass.GetGameViewPosition();
 
-            //Head Position
-            gazeModel.posHead = (gazeModel.posLeftEye + gazeModel.posRightEye) * 0.5f;
+//            //Head Position
+//            gazeModel.posHead = (gazeModel.posLeftEye + gazeModel.posRightEye) * 0.5f;
 
-            //TimeStamp of the Sample
-            gazeModel.timeStamp = sampleData.timestamp;
+//            //TimeStamp of the Sample
+//            gazeModel.timeStamp = sampleData.timestamp;
 
 
 
