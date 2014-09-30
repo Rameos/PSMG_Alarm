b003adc991b6c486555d746f7c12dc01214cee3c
dudesoft <andreas@pritschet.eu>
2014-09-30 22:07:44 +0200

    reset master branch

--- PSMG_Alarm/Assets/Scripts/Controllers/GameConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GameConstants : MonoBehaviour
-{
-    public static readonly int PLAYER_START_HEALTH = 4;
-}
--- PSMG_Alarm/Assets/Scripts/Controllers/SettignsController.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class SettignsController : MonoBehaviour {
-    public static float soundVolume;
-    public static float musicVolume;
-    public static bool useGaze;
-}
